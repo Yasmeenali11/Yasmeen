09e8d90e1ae805a9ed9adb1a095c09f6c1985b47
Yasmeenali11 <aliyasmeen675@gmail.com>
2020-09-16 14:48:51 +0300

    first commit

---
+++ part2/javascript.js
@@ -0,0 +1,20 @@
+var cat;
+var arry;
+var x=document.getElementById("name");
+var y=document.getElementById("info");
+varz=document.getElementById("imag");
+arry=0;
+
+
+
+function fun(){
+	cat={:};
+	
+	for(var i=0;i<4;i++){
+		cat[name]=document.getElementById("txt1").value;
+		cat[imag]=document.getElementById("img").value;
+        cat[name]=document.getElementById("txt2").value;
+        arry+=arry;
+	}
+
+}
